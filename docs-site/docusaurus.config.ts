6ae07d2d13ed51c58b073487a51ffc43c6714de9
jhonbrownn <JohnnieWalker@cloudnativeworks.com>
2026-08-17 17:34:05 +0300

    feat: integrate Google Tag Manager across static pages and Docusaurus configuration

--- docs-site/docusaurus.config.ts
+++ docs-site/docusaurus.config.ts
@@ -75,6 +75,7 @@ export default async function createConfig(): Promise<Config> {
           pages: false,
           theme: {customCss: './src/css/custom.css'},
           sitemap: {changefreq: 'weekly'},
+          googleTagManager: {containerId: 'GTM-TZFN48ZD'},
         } satisfies Preset.Options,
       ],
     ],
